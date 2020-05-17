bb9fb6e913eda5e5af98ff34d7488ee67d5e8cec
kwong <kirkwong33@gmail.com>
2020-05-17 01:58:12 -0700

    Adding logic to have paragraphs of at least 300 characters and added string cleanup methods

--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+import re
+
+# https://howpcrules.com/step-by-step-guide-on-scraping-data-from-a-website-and-saving-it-to-a-database/
 
 
 def browser_driver():
@@ -16,18 +19,52 @@ def browser_driver():
 
 def access_page(driver):
     url = "https://universe.leagueoflegends.com/en_US/story/perennial/"
+
     driver.get(url)
     all_p = driver.find_elements_by_xpath("//p[@class='p_1_sJ']")
-    print("length", len(all_p))
-    for p in all_p:
-        print(p.get_attribute("innerHTML"))
+
     page_source = driver.page_source
     return page_source
+
+    # for p in all_p:
+    #     print(len(p.get_attribute("innerHTML")))
 
 
 def extract_data(page_source):
     soup = BeautifulSoup(page_source, 'html.parser')
-    
+    code_soup = soup.find_all('p', attrs={'class': 'p_1_sJ'})
+
+    add_paragraph = ""
+    paragraph_list = []
+    append = False
+
+    for elem in code_soup:
+        # use get_text() to remove <i> tags
+        if not append:
+            if len(elem.get_text()) < 300:
+                add_paragraph += " "
+                add_paragraph += elem.get_text()
+                append = True
+            else:
+                paragraph_list.append(elem.get_text())
+        else:
+            add_paragraph += elem.get_text()
+            if len(add_paragraph) > 300:
+                append = False
+                paragraph_list.append(add_paragraph)
+                add_paragraph = ""
+    for index, para in enumerate(paragraph_list):
+        paragraph_list[index] = para.strip()
+        paragraph_list[index] = correct_period(para)
+    for para in paragraph_list:
+        print(len(para), para)
+
+
+def correct_period(s):
+    # r' + ' searches for multiple spaces to replace with single spaces in s
+    # re.sub(r'\.(?! )', '. ', looks for periods without following space character to replace with '. '
+    return re.sub(r'\.(?! )', '. ', re.sub(r' +', ' ', s))
+
 
 def main():
     driver = browser_driver()
